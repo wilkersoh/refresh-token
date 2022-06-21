f92ae9cd10517328db3ae3b0094b84705b29e33d
wilker <wilker@iqiglobal.com>
2022-06-21 15:50:15 +0800

    disabled component dev tool

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import App from "./App";
 import { AuthProvider } from "./context/AuthProvider";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
